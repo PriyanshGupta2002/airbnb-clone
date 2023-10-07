4436eef895b44bb04ecb6ea9226e226f9d797833
PriyanshGupta2002 <guptapriyansh23@gmail.com>
2023-10-07 10:09:40 +0530

    fixes

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -8,6 +8,8 @@ import CategoryInput from "../Inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../Inputs/CountrySelect";
 import dynamic from "next/dynamic";
+import Counter from "../Inputs/Counter";
+import ImageUpload from "../Inputs/ImageUpload";
 
 enum STEPS {
   CATEGORY = 0,
@@ -50,6 +52,9 @@ const RentModal = () => {
 
   const category = watch("category");
   const location = watch("location");
+  const guestCount = watch("guestCount");
+  const roomCount = watch("roomCount");
+  const bathroomCount = watch("bathroomCount");
   const Map = useMemo(
     () =>
       dynamic(() => import("../Map"), {
@@ -118,7 +123,43 @@ const RentModal = () => {
     );
   }
   if (step === STEPS.INFO) {
-    bodyContent = <div>We are in info</div>;
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading title="Guests" subtitle="How many guests do you allow?" />
+        <Counter
+          title="Guests"
+          subtitle="How many guests?"
+          onChange={(value) => setCustomValue("guestCount", value)}
+          value={guestCount}
+        />
+        <hr />
+        <Counter
+          title="Rooms"
+          subtitle="How many rooms do you have?"
+          onChange={(value) => setCustomValue("roomCount", value)}
+          value={roomCount}
+        />
+        <hr />
+        <Counter
+          title="Bathrooms"
+          subtitle="How many bathrooms do you have?"
+          onChange={(value) => setCustomValue("bathroomCount", value)}
+          value={bathroomCount}
+        />
+      </div>
+    );
+  }
+
+  if (step === STEPS.IMAGES) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Add a photo of your place"
+          subtitle="Show guests what your place look like!"
+        />
+        <ImageUpload />
+      </div>
+    );
   }
 
   return (
